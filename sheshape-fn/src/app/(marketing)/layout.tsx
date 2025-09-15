487ecaa8ae11f02ccb4b3a7257960f7ed96021d8
ngabonzizacedrickkennedy <cedgoldi3@gmail.com>
2025-09-15 20:11:09 +0200

    my for

--- sheshape-fn/src/app/(marketing)/layout.tsx
+++ sheshape-fn/src/app/(marketing)/layout.tsx
@@ -1,7 +1,8 @@
 // src/app/(marketing)/layout.tsx
-import { Header } from '@/components/common/Header';
-import { Footer } from '@/components/common/Footer';
-import { ScrollToTop } from '@/components/common/ScrollToTop';
+import { Header } from "@/components/common/Header";
+import { Footer } from "@/components/common/Footer";
+import { ScrollToTop } from "@/components/common/ScrollToTop";
+import { EnhancedCartProvider } from "@/context/EnhancedCartContext";
 
 export default function MarketingLayout({
   children,
@@ -9,11 +10,13 @@ export default function MarketingLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header /> {/* Add the Header component here */}
-      <main className="flex-grow">{children}</main>
-      <Footer />
-      <ScrollToTop />
-    </div>
+    <EnhancedCartProvider>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">{children}</main>
+        <Footer />
+        <ScrollToTop />
+      </div>
+    </EnhancedCartProvider>
   );
 }
